af82b343865dc9468f147d19646b2da571fc03e4
Aditi-Pathak01 <pathakaditi322@gmail.com>
2024-12-10 20:40:53 +0530

    hero section -videos added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Hero from "./Components/Hero";
 function App() {
   return (
-    <main className="relative min-h-screen w-screen overflow-x-hidden">
+    <main className="relative min-h-screen w-screen overflow-x-hidden bg-black">
       <Hero />
     </main>
   );
--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,33 +1,53 @@
-import React from "react";
-import { useState,useRef } from "react";
+import React, { useEffect, useState } from "react";
+import { useRef } from "react";
 
 
 export default function Hero() {
+  const [currIdx, setCurrIdx] = useState(1);
+  const [hasClicked, setHasClicked] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [loadedVid, setLoadedVid] = useState(0);
+  const totalVid = 4;
+  const nextVidRef = useRef(null);
 
+  const handleMiniVdClick = () => {
+    setHasClicked(true);
+    setCurrIdx((prev) => (prev % totalVid) + 1);
+  };
+  const getVidSrc = (idx) => `../../public/videos/hero-${idx}.mp4`;
+  useEffect(() => {
+    if (loadedVid === totalVid - 1) {
+      setLoading(false);
+    }
+  }, [loadedVid]);
+
+  const handleLoadedVid = () => {
+    setLoadedVid((prev) => prev + 1);
+  };
   return (
     <div className="relative h-dvh w-screen overflow-x-hidden">
       <div
         id="video-frame"
-        className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75"
+        className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg"
       >
         <div>
           <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
-            <div
-              onClick={handleMiniVdClick}
-              className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100"
-            >
+            <div onClick={handleMiniVdClick} className="origin center">
               <video
-                ref={nextVdRef}
-                src={getVideoSrc((currentIndex % totalVideos) + 1)}
+                autoPlay
+                ref={nextVidRef}
+                src={getVidSrc((currIdx % totalVid) + 1)}
                 loop
                 muted
                 id="current-video"
-                className="size-64 origin-center scale-150 object-cover object-center"
-                onLoadedData={handleVideoLoad}
+                className="size-64 origin-center scale-150 object-center object-cover"
+                onLoadedData={handleLoadedVid}
               />
             </div>
           </div>
         </div>
+
+        <video src={getVidSrc(currIdx)} />
       </div>
     </div>
   );
